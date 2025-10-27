28cef1ab353a01f1ccb415b844653564cc8a393a
BigCal42 <rory.calnan@gmail.com>
2025-10-27 15:07:21 -0400

    feat: restore react entry point

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.tsx';
+import { AppProviders } from './providers/AppProviders.tsx';
+
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </React.StrictMode>,
+);
